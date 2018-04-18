0d85cc4e29fa92d607589734c96960a4d5e87e3a
watsonzr <watsonzr@rose-hulman.edu>
2018-04-17 21:21:33 -0400

    Final Version of problem2

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 1, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zack Watson.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -154,9 +154,18 @@ def problem2(n, seq):
       :type seq:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    nums = []
+    for i in range(len(seq)):
+        if seq[i] < n:
+            nums = nums + [seq[i]]
+    if len(nums) >= 3:
+        return [nums[0], nums[1], nums[2]]
+    else:
+        return 'Too few'
 
 
 ###############################################################################
